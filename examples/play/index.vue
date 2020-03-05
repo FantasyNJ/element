b1128a6e005e988e8ae5e0219069d2eafb3a1ec6
宁杰 <ningjie@qijiayoudao.com>
2020-03-05 11:00:06 +0800

    fix time-picker bug && add some functions

---
+++ examples/play/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div style="margin: 20px;">
+    <el-time-picker
+      is-range
+      v-model="value1"
+      value-format="HH:mm"
+      format="HH:mm"
+      :picker-options="{
+        selectableRange: '04:00:00 - 23:59:00'
+      }"
+      placeholder="选择时间范围"
+      @change="change"
+    >
+    </el-time-picker>
+    <br>
+    <el-time-picker
+      v-model="value2"
+      :picker-options="{
+        selectableRange: '18:30:00 - 20:30:00'
+      }"
+      value-format="HH:mm"
+      format="HH:mm"
+      placeholder="任意时间点">
+    </el-time-picker>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value1: ['08:40', '07:40'],
+        value2: '08:40'
+      };
+    },
+    methods: {
+      change(val){
+        console.log(val)
+      }
+    }
+  };
+</script>
